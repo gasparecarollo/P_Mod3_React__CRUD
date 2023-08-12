ed650ad112503846ee0aff73bf3683cb9913e28b
Gaspare Carollo <gasparecarollo@pursuit.org>
2023-08-12 18:20:25 -0400

    React App Started; Boiler-Plate Code Removedl installed React Router V6

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+// import { useState } from 'react'
+import './App.css'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+
+function App() {
+
+
+  return (
+    <>
+      <Router>
+
+        <h1>ScreenViews</h1>
+
+      </Router>
+    </>
+  )
+}
+
+export default App
